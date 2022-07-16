7922934ca06e57704f2c317fb4911d586698567c
Abhishek Hatti <Abhishek.Hatti@amnhealthcare.com>
2022-07-16 21:58:33 +0530

    First Commit

---
+++ AmazonUITests/AmazonUITests.cs
@@ -0,0 +1,21 @@
+﻿
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AmazonUITests
+{
+    public class AmazonUITests
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+            }
+
+        }
+    }
+}
